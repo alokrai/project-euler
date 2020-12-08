4a33b9aca7e60ad69378bf51421c7c9823e762d0
alokrai <alok.rai3@me.com>
2020-12-08 17:55:56 +0100

    solved problem 10, brute force

---
+++ solution-10.js
@@ -0,0 +1,28 @@
+// The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+// Find the sum of all the primes below two million.
+
+// 142913828922
+
+// TODO: Improve the prime function,sieve of eratosthenes
+const primes = [2];
+
+function isPrime(num) {
+  for (let i = 0; i < primes.length; i++) {
+    if (num % primes[i] === 0) return false;
+  }
+  return true;
+}
+
+function generatePrimes() {
+  for (let i = 3; i < 2000000; i = i + 2) {
+    if (isPrime(i)) {
+      primes.push(i);
+      console.log(i);
+    }
+  }
+}
+
+generatePrimes();
+const total = primes.reduce((a, b) => a + b, 0);
+console.log(total);
